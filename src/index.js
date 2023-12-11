27afb3731df01ae1b5669721a9d589b6f83e5844
aldiker <aldiker@ukr.net>
2023-12-11 16:55:23 +0200

    Deleted src and started index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+function App() {
+    return <h1>Hello !!!</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<App />)
