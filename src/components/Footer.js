6d5a6aa49bc56634348722455ec910b6c196df30
aldiker <aldiker@ukr.net>
2023-12-12 16:52:41 +0200

    Added "isOpen" in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,12 @@
 function Footer() {
+    const hour = new Date().getHours()
+    console.log(hour)
+
+    const openHour = 8
+    const closeHour = 22
+    const isOpen = hour >= openHour && hour <= closeHour
+    console.log(isOpen)
+
     return (
         <footer>{new Date().toLocaleTimeString}. We're currentlt open</footer>
     )
