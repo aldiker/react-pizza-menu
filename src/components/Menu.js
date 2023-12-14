23ecc775d483e4b8d5be0fee6c5afe54cf9698d9
aldiker <aldiker@ukr.net>
2023-12-14 16:57:27 +0200

    Added sold-out class in Pizza component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,14 +2,31 @@ import pizzaData from '../data/data'
 import Pizza from './Pizza'
 
 function Menu() {
+    const pizzas = pizzaData
+    const numPizzas = pizzas.length
+
     return (
         <main className='menu'>
             <h2>Our menu</h2>
-            <ul className='pizzas'>
-                {pizzaData.map((pizza, index) => (
-                    <Pizza pizza={pizza} key={index} />
-                ))}
-            </ul>
+
+            {numPizzas > 0 ? (
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose
+                        from. All from our stone oven, all organic, all
+                        delicious.
+                    </p>
+                    <ul className='pizzas'>
+                        {pizzaData.map((pizza, index) => (
+                            <Pizza pizza={pizza} key={index} />
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <p>
+                    We're still working on our menu. Please come back later ...
+                </p>
+            )}
         </main>
     )
 }
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,15 +1,13 @@
 function Pizza(props) {
     const { pizza } = props
 
-    if (pizza.soldOut) return null
-
     return (
-        <li className='pizza'>
+        <li className={`pizza ${pizza.soldOut ? 'sold-out' : ''}`}>
             <img src={pizza.photoName} alt='{pizza.name}' />
             <div>
                 <h3>{pizza.name}</h3>
                 <p>{pizza.ingredients}</p>
-                <span>{pizza.price}</span>
+                <span>{pizza.soldOut ? 'SOLD OUT' : pizza.price}</span>
             </div>
         </li>
     )
